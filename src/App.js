5e7cd624cf96300eb8e3f890b6648b923bd57998
enya <essidanaya@gmail.com>
2019-06-29 14:09:49 +0300

    Login component, login routing was created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './FooterComponent.jsx';
 import Home from './HomeComponent.jsx';
 import CatalogComponent from './CatalogComponent.jsx';
 import { BrowserRouter, Route } from 'react-router-dom';
+import LoginCard from "./LoginComponent";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
             <Header/>
             <Route exact path ="/" component = {Home}/>
             <Route path = "/catalog" component = {CatalogComponent}/>
+            <Route path = "/login" component = {LoginCard}/>
             <Footer/>
         </div>
         </BrowserRouter>
