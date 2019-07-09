6a2abf4e6630894800b4c95411ae81f91340a4ca
enya <essidanaya@gmail.com>
2019-07-09 19:23:57 +0300

    some changes was added

--- src/CatalogComponent.jsx
+++ src/CatalogComponent.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import CardComponent from "./CardComponent.jsx";
 
-function CatalogComponent () {
-
-    return (
+const CatalogComponent = () => (
         <div>
-            <CardComponent/>
-            <CardComponent/>
-            <CardComponent/>
+            {[  <CardComponent/>,
+                <CardComponent/>,
+                <CardComponent/>,
+                <CardComponent/>,
+                <CardComponent/>,
+                <CardComponent/>
+            ].map(item => <li key={item}>{item}</li>)}
         </div>
     );
-}
 
 export default CatalogComponent;
